3909201e2b269ab9a59b04ec609287551632551e
rubelcste <rubel.nstu27@gmail.com>
2019-12-12 16:48:52 +0600

    Authories Products Updated & destroy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-    //     factory(App\User::class, 50)->create()->each(function ($user) {
-    //     $user->posts()->save(factory(App\Post::class)->make());
-    // });
+        factory(App\User::class,10)->create();
     	factory(App\Model\Product::class,50)->create();
         factory(App\Model\Review::class,300)->create();
     }
